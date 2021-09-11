944a17792c9da0c6e3b28ab800ed8b261396fbc0
Ilya Ozerov <arnage92@gmail.com>
2021-09-11 14:38:42 +0300

    fix ports

--- up_explorer.sh
+++ up_explorer.sh
@@ -1,1 +1,1 @@
-docker run --rm -it --name polkadot-ui -e WS_URL=ws://localhost:9944 -p 4000:4000 jacogr/polkadot-js-apps:latest
+docker run --rm -it --name polkadot-ui -e WS_URL=ws://localhost:9944 -p 80:80 jacogr/polkadot-js-apps:latest
